078b0656cfa692ef26a07a3dc89f8c184fd8a55e
NicMortelliti <npmortelliti@gmail.com>
2022-08-02 16:11:13 -0700

    Fixed create method in controller

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,18 +1,8 @@
 class Movie < ApplicationRecord
-  CATEGORIES = [
-    'Comedy',
-    'Drama',
-    'Animation',
-    'Mystery',
-    'Horror',
-    'Fantasy',
-    'Action',
-    'Documentary',
-    'Science Fiction'
-  ]
+  CATEGORIES = ['Comedy', 'Drama', 'Animation', 'Mystery', 'Horror', 'Fantasy', 'Action', 'Documentary', 'Science Fiction']
 
   validates :title, presence: true
-  validates :year, numericality: { 
+  validates :year, numericality: {
     greater_than_or_equal_to: 1888,
     less_than_or_equal_to: Date.today.year
   }
